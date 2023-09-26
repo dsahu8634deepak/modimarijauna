3ee73037d715cbdbec3af3f981cf5c291c54c3ab
deepakSahu <121280456+dsahu8634deepak@users.noreply.github.com>
2023-09-26 23:04:15 +0530

    code

---
+++ assets/js/index.js
@@ -0,0 +1,18 @@
+// // include Header
+// $(function () {
+//     $("#header").load("layout/header.html");
+//     $("#footer").load("layout/footer.html");
+// });
+// // include Header
+// header color change
+
+// header color change
+
+
+
+
+
+
+
+
+
